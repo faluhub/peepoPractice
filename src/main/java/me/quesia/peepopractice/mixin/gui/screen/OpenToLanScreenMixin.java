25681cf78e1da5637a36358b67057acff679d9d2
wurgo <laxvideoeffects@gmail.com>
2023-02-20 20:34:59 +0000

    fix pause crash

--- src/main/java/me/quesia/peepopractice/mixin/gui/screen/OpenToLanScreenMixin.java
+++ src/main/java/me/quesia/peepopractice/mixin/gui/screen/OpenToLanScreenMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(OpenToLanScreen.class)
 public class OpenToLanScreenMixin {
-    @Redirect(method = "method_19851", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/integrated/IntegratedServer;openToLan(Lnet/minecraft/world/GameMode;ZI)Z"), remap = false)
+    @Redirect(method = "method_19851", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/integrated/IntegratedServer;openToLan(Lnet/minecraft/world/GameMode;ZI)Z"))
     private boolean disableLan(IntegratedServer instance, GameMode gameMode, boolean cheatsAllowed, int port) {
         if (!PeepoPractice.CATEGORY.equals(PracticeCategories.EMPTY)) {
             return true;
