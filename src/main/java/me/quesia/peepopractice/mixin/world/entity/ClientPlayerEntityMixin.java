cf1ddf3baa626b4bfbff381448767ab5e4930dd3
Maya <71964154+QuesiaSR@users.noreply.github.com>
2023-03-20 11:57:56 +0100

    remove unnecessary space

--- src/main/java/me/quesia/peepopractice/mixin/world/entity/ClientPlayerEntityMixin.java
+++ src/main/java/me/quesia/peepopractice/mixin/world/entity/ClientPlayerEntityMixin.java
@@ -8,7 +8,6 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ClientPlayerEntity.class)
 public abstract class ClientPlayerEntityMixin {
-
     @ModifyReturnValue(method = "showsDeathScreen", at = @At("RETURN"))
     private boolean peepoPractice$noDeathScreen(boolean showsDeathScreen) {
         return showsDeathScreen && !PeepoPractice.CATEGORY.hasSplitEvent();
