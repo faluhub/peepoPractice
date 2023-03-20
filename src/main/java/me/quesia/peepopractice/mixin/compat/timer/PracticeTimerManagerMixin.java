339a8d7c5a0b3fb541e4f696e4fa75b96c9e7d3b
Maya <71964154+QuesiaSR@users.noreply.github.com>
2023-03-20 11:15:29 +0100

    remove unnecessary space

--- src/main/java/me/quesia/peepopractice/mixin/compat/timer/PracticeTimerManagerMixin.java
+++ src/main/java/me/quesia/peepopractice/mixin/compat/timer/PracticeTimerManagerMixin.java
@@ -10,7 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = PracticeTimerManager.class, remap = false)
 public abstract class PracticeTimerManagerMixin {
-
     @Inject(method = "startPractice", at = @At("HEAD"), cancellable = true)
     private static void peepoPractice$disablePracticeTimer(CallbackInfo ci) {
         if (!PeepoPractice.CATEGORY.equals(PracticeCategories.EMPTY)) {
