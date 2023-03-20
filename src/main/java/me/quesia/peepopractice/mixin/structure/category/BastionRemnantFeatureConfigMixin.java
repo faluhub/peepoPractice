7fbf6f0239a1bed31dd529f0957b816433f13203
Maya <71964154+QuesiaSR@users.noreply.github.com>
2023-03-20 11:37:46 +0100

    remove unnecessary space

--- src/main/java/me/quesia/peepopractice/mixin/structure/category/BastionRemnantFeatureConfigMixin.java
+++ src/main/java/me/quesia/peepopractice/mixin/structure/category/BastionRemnantFeatureConfigMixin.java
@@ -16,7 +16,6 @@ import java.util.List;
 
 @Mixin(BastionRemnantFeatureConfig.class)
 public abstract class BastionRemnantFeatureConfigMixin {
-
     @Shadow @Final private List<StructurePoolFeatureConfig> possibleConfigs;
 
     @ModifyReturnValue(method = "getRandom", at = @At("RETURN"))
