030ead259f5d7a06b3613e53fc77bad577defd1f
Maya <71964154+QuesiaSR@users.noreply.github.com>
2023-03-20 11:08:34 +0100

    remove unnecessary space

--- src/main/java/me/quesia/peepopractice/mixin/compat/KeyboardMixin.java
+++ src/main/java/me/quesia/peepopractice/mixin/compat/KeyboardMixin.java
@@ -9,7 +9,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = Keyboard.class, priority = 1005)
 public abstract class KeyboardMixin {
-
     @Inject(method = "onKey", at = @At("HEAD"))
     private void peepoPractice$preventAtumReset(CallbackInfo ci) {
         PeepoPractice.disableAtumKey();
