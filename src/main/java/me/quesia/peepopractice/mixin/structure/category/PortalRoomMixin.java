fca5ca3a716622e9fe3ece9ca3a4014ccad62ca9
Maya <71964154+QuesiaSR@users.noreply.github.com>
2023-03-20 11:39:41 +0100

    Update PortalRoomMixin.java

--- src/main/java/me/quesia/peepopractice/mixin/structure/category/PortalRoomMixin.java
+++ src/main/java/me/quesia/peepopractice/mixin/structure/category/PortalRoomMixin.java
@@ -9,7 +9,6 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(StrongholdGenerator.PortalRoom.class)
 public abstract class PortalRoomMixin {
-
     @ModifyConstant(method = "generate(Lnet/minecraft/world/ServerWorldAccess;Lnet/minecraft/world/gen/StructureAccessor;Lnet/minecraft/world/gen/chunk/ChunkGenerator;Ljava/util/Random;Lnet/minecraft/util/math/BlockBox;Lnet/minecraft/util/math/ChunkPos;Lnet/minecraft/util/math/BlockPos;)Z", constant = @Constant(floatValue = 0.9F))
     private float peepoPractice$setEyeCount(float constant) {
         PracticeTypes.EyeCountType eyeCountType = PracticeTypes.EyeCountType.fromLabel(CategoryPreference.getValue("eye_count"));
