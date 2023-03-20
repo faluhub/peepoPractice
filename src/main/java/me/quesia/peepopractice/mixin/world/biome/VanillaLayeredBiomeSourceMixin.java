351384a461ab1ab5d03c14e1e001cd97fc2dc4dc
Maya <71964154+QuesiaSR@users.noreply.github.com>
2023-03-20 11:57:32 +0100

    Update VanillaLayeredBiomeSourceMixin.java

--- src/main/java/me/quesia/peepopractice/mixin/world/biome/VanillaLayeredBiomeSourceMixin.java
+++ src/main/java/me/quesia/peepopractice/mixin/world/biome/VanillaLayeredBiomeSourceMixin.java
@@ -14,25 +14,17 @@ import java.util.Map;
 
 @Mixin(VanillaLayeredBiomeSource.class)
 public abstract class VanillaLayeredBiomeSourceMixin {
-
     @ModifyReturnValue(method = "getBiomeForNoiseGen", at = @At("RETURN"))
     private Biome peepoPractice$antiBiomeLogic(Biome biome, int biomeX, int biomeY, int biomeZ) {
         if (PeepoPractice.CATEGORY.hasWorldProperties()) {
-            {
-                for (Map.Entry<Biome, Integer> entry : PeepoPractice.CATEGORY.getWorldProperties().getAntiBiomeRangeMap().entrySet()) {
-                    if (entry.getValue() == null || new BlockPos(biomeX, biomeY, biomeZ).isWithinDistance(new Vec3i(0, 62, 0), entry.getValue())) {
-                        return Biomes.PLAINS;
-                    }
+            for (Map.Entry<Biome, Integer> entry : PeepoPractice.CATEGORY.getWorldProperties().getAntiBiomeRangeMap().entrySet()) {
+                if (entry.getValue() == null || new BlockPos(biomeX, biomeY, biomeZ).isWithinDistance(new Vec3i(0, 62, 0), entry.getValue())) {
+                    return Biomes.PLAINS;
                 }
             }
-            {
-                for (Map.Entry<Biome, Integer> entry : PeepoPractice.CATEGORY.getWorldProperties().getProBiomeRangeMap().entrySet()) {
-                    if (entry.getValue() == null) {
-                        break;
-                    } else if (new BlockPos(biomeX, biomeY, biomeZ).isWithinDistance(new Vec3i(0, 62, 0), entry.getValue())) {
-                        return entry.getKey();
-                    }
-                }
+            for (Map.Entry<Biome, Integer> entry : PeepoPractice.CATEGORY.getWorldProperties().getProBiomeRangeMap().entrySet()) {
+                if (entry.getValue() == null) { break; }
+                else if (new BlockPos(biomeX, biomeY, biomeZ).isWithinDistance(new Vec3i(0, 62, 0), entry.getValue())) { return entry.getKey(); }
             }
         }
         return biome;
