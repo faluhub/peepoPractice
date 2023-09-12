5ccc9b8c14a401d55d1a2d9818c34723dcd2f6b7
wurgo <laxvideoeffects@gmail.com>
2023-09-12 21:50:31 +0100

    fix biome forcing order issue

--- src/main/java/me/falu/peepopractice/mixin/world/biome/MultiNoiseBiomeSourceMixin.java
+++ src/main/java/me/falu/peepopractice/mixin/world/biome/MultiNoiseBiomeSourceMixin.java
@@ -17,19 +17,19 @@ public abstract class MultiNoiseBiomeSourceMixin {
     @ModifyReturnValue(method = "getBiomeForNoiseGen", at = @At("RETURN"))
     private Biome peepoPractice$antiBiomeLogic(Biome biome, int biomeX, int biomeY, int biomeZ) {
         if (PeepoPractice.CATEGORY.hasWorldProperties()) {
-            for (WorldProperties.BiomeModification entry : PeepoPractice.CATEGORY.getWorldProperties().getAntiBiomes()) {
-                if (entry.getRange().isValidDimension(World.NETHER)) {
-                    if (entry.isInfinite() || new BlockPos(biomeX, biomeY, biomeZ).isWithinDistance(new Vec3i(0, 62, 0), entry.getRange().getRange())) {
-                        return entry.hasReplacement() ? entry.getReplacement() : Biomes.NETHER_WASTES;
-                    }
-                }
-            }
             for (WorldProperties.BiomeModification entry : PeepoPractice.CATEGORY.getWorldProperties().getProBiomes()) {
                 if (entry.getRange().isValidDimension(World.NETHER)) {
                     if (entry.isInfinite() || new BlockPos(biomeX, biomeY, biomeZ).isWithinDistance(new Vec3i(0, 62, 0), entry.getRange().getRange())) {
                         if (entry.getRange().shouldPlace()) {
                             return entry.getBiome();
                         }
+                    }
+                }
+            }
+            for (WorldProperties.BiomeModification entry : PeepoPractice.CATEGORY.getWorldProperties().getAntiBiomes()) {
+                if (entry.getRange().isValidDimension(World.NETHER)) {
+                    if (entry.isInfinite() || new BlockPos(biomeX, biomeY, biomeZ).isWithinDistance(new Vec3i(0, 62, 0), entry.getRange().getRange())) {
+                        return entry.hasReplacement() ? entry.getReplacement() : Biomes.NETHER_WASTES;
                     }
                 }
             }
