63924ca694d4e497cca45a94d1e9872326b8186f
wurgo <laxvideoeffects@gmail.com>
2023-09-12 18:22:32 +0100

    fix weird rendering bug

--- src/main/java/me/falu/peepopractice/mixin/gui/renderer/ItemRendererMixin.java
+++ src/main/java/me/falu/peepopractice/mixin/gui/renderer/ItemRendererMixin.java
@@ -4,12 +4,15 @@ import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ItemRenderer.class)
 public abstract class ItemRendererMixin {
+    @Shadow public float zOffset;
+
     @Unique
     private static boolean shouldScale(ItemStack stack) {
         return stack.getTag() != null && (stack.getTag().contains("MaxCount") || stack.getTag().contains("MinCount"));
@@ -18,7 +21,7 @@ public abstract class ItemRendererMixin {
     @ModifyArg(method = "renderGuiItemModel", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;translatef(FFF)V", ordinal = 0), index = 2)
     private float peepoPractice$bringForward(float z, @Local(argsOnly = true) ItemStack stack) {
         if (shouldScale(stack)) {
-            return 0.0F;
+            return this.zOffset;
         }
         return z;
     }
